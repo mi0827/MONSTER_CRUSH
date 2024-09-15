9291653b0e7eacbaba8ec9b7f04c53100984f3c3
0827 <sansenjunye@gamil.com>
2024-09-15 15:11:57 +0900

    エンドシーンにもコピーを適用済み

--- src/Scene/EndScene.cpp
+++ src/Scene/EndScene.cpp
@@ -1,28 +1,77 @@
 #include "src/WinMain.h"
+#include "src/System/Vector3.h"
 #include "src/System/Vector2.h"
+#include "src/System/Transform.h"
+
+
+#include "src/Model/Model.h"
+#include "src/Animation/Animation.h"
+#include "src/Effect/Effect.h"
+
+#include "src/Collision/BoxCollision.h"
+#include "src/Collision/CapsuleCollision.h"
+#include "src/Hit/Hit.h"
+
+#include "src/System/UIBar.h"
+
+#include "src/Action/Combo.h"
+#include "src/System/Move.h"
+#include "src/Character/CharacterBase.h"
+#include "src/Character/SamplePlayer.h"
+
+#include "src/Field/FieldBase.h"
+#include "src/Field/HitField.h" 
+#include "src/Field/TitleField.h"
+
+
 #include "src/Sound/BGM.h"
 #include "src/Sound/SE.h"
+
+#include "src/Camera.h"
+
 #include "Base/Scene_Base.h"
 #include "EndScene.h"
 
-BGM end_bgm;
 
-const int End_Time_MAX = 3;  // エンド描画時間(今だけ3秒)
+
 //------------------------------------------
 // 初期処理
 //------------------------------------------
 void EndScene::Init()
 {
+	// フィールドの初期化
+	field.Init();
 
 
+	// カメラの初期設定
+	camera.PlayField_Init();
+
+	// プレイヤーの設定
+	player = new SamplePlayer;
+
+	// プリえやーの初期設定 
+	player->Init();
+
+	// シャドーマップの設定
+	ShadowMapInit();
 }
+
 
 //------------------------------------------
 // 更新処理
 //------------------------------------------
 void EndScene::Update()
 {
-	
+
+	// プレイヤーの更新処理
+	player->Update(&camera.m_rot);
+	// フィールドの地面モデルとキャラクターの当たり判定
+	HitGroundCharacter(&player->m_transform.pos, &field.m_field_model);
+
+	// カメラの更新処理
+	camera.Update(&player->m_transform.pos);
+
+
 	// Xキーを押された時にシーンの変更をする（今だけの仮）
 	if (PushHitKey(KEY_INPUT_RETURN))
 	{
@@ -32,6 +81,7 @@ void EndScene::Update()
 	{
 		m_scene_change_judge = false;
 	}
+
 }
 
 //------------------------------------------
@@ -39,7 +89,64 @@ void EndScene::Update()
 //------------------------------------------
 void EndScene::Draw()
 {
-	DrawString(100, 100, "終わり", GetColor(255, 255, 255));
+	// プレイヤーのシャドーマップのエリアのセット
+	SetPlayerShadowMapArea(player->m_transform.pos);
+
+	//-------------------------------------------------------------
+		// シャドウマップの作成（ここで各オブジェクトのシャドーマップの設定）
+		//-------------------------------------------------------------
+		// シャドウマップへの描画の準備
+	ShadowMap_DrawSetup(m_player_shadowMap_handle);
+	{
+		// プレイヤーの描画処理
+		player->Draw();
+
+		// ヒーローの描画処理
+		//hero.Draw();
+	}
+	ShadowMap_DrawSetup(m_shadowMap_handle);
+	{
+		// シャドウマップへキャラクターモデルの描画
+		//MV1SetPosition(ground, VGet(0.0f, 0.0f, 0.0f)); // 描画するプレイヤーモデルの座標の設定
+		//MV1SetRotationXYZ(ground, VGet(TO_RADIAN(0.0f), TO_RADIAN(0.0f), TO_RADIAN(0.0f))); // モデルの回転
+		//MV1SetScale(ground, VGet(10, 10, 10)); // モデルの大きさ(10分の１のサイズ)
+		//MV1DrawModel(ground); // モデルの描画
+		field.Draw();
+
+	}
+
+	// シャドウマップへの描画を終了
+	ShadowMap_DrawEnd();
+
+	//-------------------------------------------------------------
+	// 各モデルの描画
+	//-------------------------------------------------------------
+
+	// バックバッファに描画する
+	SetDrawScreen(DX_SCREEN_BACK);
+
+	// カメラの描画処理
+	camera.Draw();
+
+
+	// 描画に使用するシャドウマップを設定
+	SetUseShadowMap(1, m_player_shadowMap_handle);
+	{
+		player->Draw();
+
+		// ヒーローの描画処理
+		//hero.Draw();
+	}
+	SetUseShadowMap(0, m_shadowMap_handle);
+	{
+		// シャドウマップへキャラクターモデルの描画
+		field.Draw();
+
+	}
+	UseShadowMapSet();
+
+	DrawString(100, 100, "クエストエリア :: RENTER", GetColor(255, 255, 255));
+
 }
 
 //------------------------------------------
@@ -47,12 +154,8 @@ void EndScene::Draw()
 //------------------------------------------
 void EndScene::Exit()
 {
+	//　シャドーマップの削除
+	ExitShadowMap();
 
 }
-//------------------------------------------
-// BGMの初期処理
-//------------------------------------------
-void EndScene::BGM_Init()
-{
-	
-}
+
